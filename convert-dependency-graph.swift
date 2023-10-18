8f48aebb37c358b56fadfb29c836dd20a14fc308
Gwynne Raskind <gwynne@vapor.codes>
2023-10-18 14:12:28 -0500

    [skip ci] Update to fix output

--- convert-dependency-graph.swift
+++ convert-dependency-graph.swift
@@ -9,6 +9,7 @@ struct PackageDependency: Codable {
 
 struct SwiftPUrl: Codable, RawRepresentable {
     let scheme: String, type: String, source: String, name: String, version: String
+    
     var rawValue: String { "\(self.scheme):\(self.type)/\(self.source)/\(self.name)@\(self.version)" }
     
     init?(rawValue raw: String) {
@@ -33,21 +34,28 @@ struct GithubDependencyGraph: Codable {
     struct Manifest: Codable {
         struct File: Codable { let source_location: String }
         struct Package: Codable {
-            let package_url: SwiftPUrl, scope: String, dependencies: [String]
-            init(package_url: SwiftPUrl, dependencies: [String]) {
-                (self.package_url, self.scope, self.dependencies) = (package_url, "runtime", dependencies)
+            let package_url: SwiftPUrl, scope: String, relationship: String, dependencies: [String]
+            init(package_url: SwiftPUrl, dependencies: [String], relationship: String) {
+                self.package_url = package_url
+                self.scope = "runtime"
+                self.dependencies = dependencies
+                self.relationship = relationship
             }
         }
-        let name: String, file: File, resolved: [String: Package]
+        let name: String, file: File, resolved: [SwiftPUrl: Package]
     }
     let version: Int, sha: String, ref: String, job: Job, detector: Detector,
         scanned: Date, manifests: [String: Manifest]
 }
 
+func fail(_ message: @autoclosure () -> String) -> Never {
+    try? FileHandle.standardError.write(contentsOf: Array("\(message())\n".utf8))
+    exit(1)
+}
+
 func env(_ name: String) -> String {
     guard let value = ProcessInfo.processInfo.environment[name] else {
-        try? FileHandle.standardError.write(contentsOf: Array("Incomplete environment: \(name).\n".utf8))
-        exit(1)
+        fail("Incomplete environment: \(name)")
     }
     return value
 }
@@ -58,42 +66,45 @@ func main() {
     encoder.dateEncodingStrategy = .iso8601
     encoder.outputFormatting = [.withoutEscapingSlashes, .sortedKeys]
 
-    let branch = env("BRANCH"), commit = env("COMMIT"), correlator = env("CORRELATOR"),
-        runId = env("RUN_ID"), detector = env("GITHUB_ACTION"), detectorVer = env("GITHUB_ACTION_REF"),
-        detectorRepo = env("GITHUB_ACTION_REPOSITORY"), serverUrl = env("GITHUB_SERVER_URL")
+    let branch = env("BRANCH"),
+        commit = env("COMMIT"),
+        correlator = env("CORRELATOR"),
+        runId = env("GITHUB_RUN_ID"),
+        detectorVer = env("GITHUB_ACTION_REF"),
+        serverUrl = env("GITHUB_SERVER_URL")
     
-    let dependencies = try! decoder.decode(
+    let topLevelDependencies = try! decoder.decode(
         PackageDependency.self,
         from: FileHandle.standardInput.readToEnd() ?? .init()
     ).dependencies
     
-    var resolved = [String: GithubDependencyGraph.Manifest.Package]()
+    var resolved = [SwiftPUrl: GithubDependencyGraph.Manifest.Package]()
     
     func handleDeps(_ dependencies: [PackageDependency]) {
         for dep in dependencies where !resolved.keys.contains(dep.identity) {
             handleDeps(dep.dependencies)
             guard !resolved.keys.contains(dep.identity) else { continue }
             guard let url = URL(string: dep.url) else {
-                try? FileHandle.standardError.write(contentsOf: Array("Invalid URL for package \(dep.identity)\n".utf8))
-                exit(1)
+                fail("Invalid URL for package \(dep.identity)")
             }
-            resolved[dep.identity] = .init(
+            resolved[.init(with: url, version: dep.version)] = .init(
                 package_url: .init(with: url, version: dep.version),
                 dependencies: dep.dependencies.map {
                     SwiftPUrl(with: URL(string: $0.url)!, version: $0.version).rawValue
-                }.sorted()
+                }.sorted(),
+                relationship: topLevelDependencies.map(\.identity).contains(dep.identity) ? "direct" : "indirect"
             )
         }
     }
-    handleDeps(dependencies)
+    handleDeps(topLevelDependencies)
 
     let graph = GithubDependencyGraph(
         version: 0, sha: commit, ref: branch,
         job: .init(correlator: correlator, id: runId),
         detector: .init(
-            name: .init(detector.drop(while: { $0 == "_" }).prefix(while: { $0 != "_" })),
+            name: "vapor-community/swift-dependency-submission",
             version: detectorVer.isEmpty ? "v0" : detectorVer,
-            url: "\(serverUrl)/\(detectorRepo.isEmpty ? "vapor/ci" : detectorRepo)"
+            url: "\(serverUrl)/vapor-community/swift-dependency-submission"
         ),
         scanned: Date(),
         manifests: ["Package.resolved": .init(
